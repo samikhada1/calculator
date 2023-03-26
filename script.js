c8dadda492b47a7fe6d675e468100c07e44d3ccd
samikhad <usmanovsn331@gmail.com>
2023-03-27 00:15:43 +0500

    sakjdjfna

--- script.js
+++ script.js
@@ -15,15 +15,25 @@ class Calculator {
 
     }
     appendNumber(number) {
-        
+        if(number === '.' && this.curentOperand.includes('.')) return
+        this.curentOperand = this.curentOperand.toString() + number.toString()
     }
     chooseOperation(operation) {
+        if(this.curentOperand === '') return;
+        if(this.previousOperand !== '') {
+            this.compute();
+        }
+        this.operation = operation
+        this.previousOperand = this.curentOperand 
+        this.curentOperand = ''
 
     }
     compute() {
 
     }
     updataDisplay() {
+        this.curentOperandTextElement.innerText = this.curentOperand
+        this.previousOperandTextElement.innerText = this.previousOperand
 
     }
 }
@@ -36,4 +46,18 @@ const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const curentOperandTextElement = document.querySelector('[data-curent-operand]')
 
-s
+const calculator = new Calculator(previousOperandTextElement, curentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () =>{
+        calculator.appendNumber(button.innerText)
+        calculator.updataDisplay()
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () =>{
+        calculator.chooseOperation(button.innerText)
+        calculator.updataDisplay()
+    })
+})
